0d7659f26e295f67e2d8745df65951b8f9f92214
Jacob Asper <jacobasper191@gmail.com>
2024-08-14 22:11:12 -0400

    update with generic duration

--- src/components/progress.rs
+++ src/components/progress.rs
@@ -1,18 +1,32 @@
-use core::time::Duration;
+use core::time::{self, Duration};
 
 use leptonic::prelude::*;
 use leptos::*;
 
 #[component]
-pub fn TimedProgressBar(#[prop(default = Some(0.))] start: Option<f64>) -> impl IntoView {
+pub fn TimedProgressBar(
+    #[prop(default = Some(0.))] start: Option<f64>,
+    length: Duration,
+) -> impl IntoView {
     let (progress, set_progress) = create_signal(Some(start.unwrap_or_default()));
+
+    let interval = length.div_f32(100.);
 
     set_interval(
         move || {
             set_progress.update(|p| *p = p.map(|v| v + 1.));
         },
-        Duration::from_secs(1),
+        interval,
     );
 
-    view! { <ProgressBar progress=progress/> }
+    let seconds_str = if length.as_secs() == 0 {
+        "".into()
+    } else {
+        format!("{} seconds,", length.as_secs())
+    };
+
+    view! {
+        <H2>{format!("length: {seconds_str} {} milliseconds", length.subsec_millis())}</H2>
+        <ProgressBar progress=progress/>
+    }
 }
--- src/pages/welcome.rs
+++ src/pages/welcome.rs
@@ -1,3 +1,5 @@
+use core::time::Duration;
+
 use leptonic::prelude::*;
 use leptos::*;
 
@@ -8,7 +10,8 @@ pub fn Welcome() -> impl IntoView {
     view! {
         <Box style="display: flex; flex-direction: column; align-items: center; padding: 1em; min-height: 100%; min-width: 100%">
             <H2>"Welcome to Leptonic"</H2>
-            <TimedProgressBar/>
+            <TimedProgressBar length=Duration::from_secs(10)/>
+            <TimedProgressBar length=Duration::from_millis(60)/>
         </Box>
     }
 }
